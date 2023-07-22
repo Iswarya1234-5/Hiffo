fe3da6d78eb80babc035c3a36605d77992a6dd32
Iswarya1234-5 <139691087+Iswarya1234-5@users.noreply.github.com>
2023-07-22 17:57:54 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,17 @@
+let person1={
+    fname:"vishnu",
+    age:25,
+    address:"cuddalore",
+    
+
+}
+let Tree
+={
+    fruit:"Mango",
+    age:18,
+    address:"cuddalore",   
+    
+}
+console.log(Tree.fruit);
+console.log(Tree.age);
+console.log(Tree.address);
